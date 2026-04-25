80d59ea57e8edbf298cd1c7b06b527cc36c0e3be
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2026-04-25 14:39:33 +0300

    Update handle_request to return status code 405 for unsupported HTTP methods

--- src/utils.cpp
+++ src/utils.cpp
@@ -10,12 +10,18 @@ http::response<http::string_body> handle_request(const http::request<http::strin
   res.set(http::field::server, "http-beastie");
   res.set(http::field::content_type, "text/html");
 
-  if (req.method() == http::verb::get && req.target() == "/") {
-    res.result(http::status::ok);
-    res.body() = "<h1 style=\"text-align: center;\">CSCE 1102</h1>";
+  if (req.method() == http::verb::get) {
+    if (req.target() == "/") {
+      res.result(http::status::ok);
+      res.body() = "<h1 style=\"text-align: center;\">CSCE 1102</h1>";
+    } else {
+      res.result(http::status::not_found);
+      res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
+    }
   } else {
-    res.result(http::status::not_found);
-    res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
+    res.result(http::status::method_not_allowed);
+    res.set(http::field::allow, "GET");
+    res.body() = "<h1 style=\"text-align: center;\">405 Method Not Allowed</h1>";
   }
 
   res.prepare_payload();
