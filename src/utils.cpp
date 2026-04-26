c577c2e8087efd18fd41e3a8d668c9700db2d57a
Rola <rolaelreedy@aucegypt.edu>
2026-04-26 18:55:10 +0300

    Serve index.html at root endpoint

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,44 +1,45 @@
-#include "utils.hpp"
+#include <boost/beast/http.hpp>
 #include <fstream>
 #include <sstream>
+#include <string>
+
+namespace http = boost::beast::http;
 
 std::string read_file(const std::string& path) {
     std::ifstream file(path);
     std::stringstream buffer;
-    
+
     if (!file.is_open()) {
-        return "File not found";
+        return "<h1>404 Not Found</h1>";
     }
 
     buffer << file.rdbuf();
     return buffer.str();
 }
-namespace beast = boost::beast;
-namespace http = beast::http;
 
-http::response<http::string_body> handle_request(const http::request<http::string_body>& req) {
-  http::response<http::string_body> res;
+http::response<http::string_body> handle_request(
+    const http::request<http::string_body>& req)
+{
+    http::response<http::string_body> res;
 
-  res.version(req.version());
-  res.keep_alive(false);
+    res.version(req.version());
+    res.set(http::field::server, "Beast");
+    res.set(http::field::content_type, "text/html");
+    res.keep_alive(req.keep_alive());
 
-  res.set(http::field::server, "http-beastie");
-  res.set(http::field::content_type, "text/html");
-
-  if (req.method() == http::verb::get) {
-    if (req.target() == "/") {
-      res.result(http::status::ok);
-      res.body() = "<h1 style=\"text-align: center;\">CSCE 1102</h1>";
-    } else {
-      res.result(http::status::not_found);
-      res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
+    if (req.method() == http::verb::get && req.target() == "/") {
+        res.result(http::status::ok);
+        res.body() = read_file("static/index.html");
+    } 
+    else if (req.method() == http::verb::get && req.target() == "/elreedy") {
+        res.result(http::status::ok);
+        res.body() = read_file("static/elreedy.html");
+    } 
+    else {
+        res.result(http::status::not_found);
+        res.body() = "<h1>404 Not Found</h1>";
     }
-  } else {
-    res.result(http::status::method_not_allowed);
-    res.set(http::field::allow, "GET");
-    res.body() = "<h1 style=\"text-align: center;\">405 Method Not Allowed</h1>";
-  }
 
-  res.prepare_payload();
-  return res;
+    res.prepare_payload();
+    return res;
 }
