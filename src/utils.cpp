f146bb24be33dae481754df8b846749616b526ee
Khaled Dawoud <kdawoud56@gmail.com>
2026-04-29 21:15:03 +0300

    Assignment 3

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,7 +1,24 @@
 #include "utils.hpp"
 
+#include <fstream>
+#include <string>
+
 namespace beast = boost::beast;
 namespace http = beast::http;
+
+std::string read_file(std::string file_name) {
+  std::string output = "";
+  std::ifstream file(file_name);
+  if (file.is_open()) {
+    std::string line = "";
+    while (getline(file, line)) {
+      output += line + "\n";
+    }
+    file.close();
+  }
+
+  return output;
+}
 
 http::response<http::string_body> handle_request(const http::request<http::string_body>& req) {
   http::response<http::string_body> res;
@@ -15,7 +32,20 @@ http::response<http::string_body> handle_request(const http::request<http::strin
   if (req.method() == http::verb::get) {
     if (req.target() == "/") {
       res.result(http::status::ok);
-      res.body() = "<h1 style=\"text-align: center;\">CSCE 1102</h1>";
+      res.body() = read_file("static/index.html");
+    } else if (req.target() == "/dawoud") {
+      res.result(http::status::ok);
+      res.body() = read_file("static/dawoud.html");
+    } else if (req.target().starts_with("/static/")) {
+      // the html I'm using loads images so I needed to make the server actually send them
+      std::string path = "." + std::string(req.target());
+
+      if (path.ends_with(".jpeg") || path.ends_with(".jpg")) {
+        res.set(http::field::content_type, "image/jpeg");
+      }
+
+      res.result(http::status::ok);
+      res.body() = read_file(path);
     } else {
       res.result(http::status::not_found);
       res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
