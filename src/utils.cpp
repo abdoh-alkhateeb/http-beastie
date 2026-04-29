0fabf11553c51e54bb7291498b6aeb7769ce76c9
Youssef Micheal <YoussefMicheal@aucegypt.edu>
2026-04-29 21:29:51 +0300

    Created botros.html with a simple bio about tennis and I implemented a new else if block in utils.cpp to access the webpage with the new html I created

--- src/utils.cpp
+++ src/utils.cpp
@@ -13,29 +13,34 @@ std::string read_file_to_string(const std::string& path) {
     buffer << file.rdbuf();
     return buffer.str();
 }
+
 http::response<http::string_body> handle_request(const http::request<http::string_body>& req) {
-  http::response<http::string_body> res;
+    http::response<http::string_body> res;
 
-  res.version(req.version());
-  res.keep_alive(false);
+    res.version(req.version());
+    res.keep_alive(false);
 
-  res.set(http::field::server, "http-beastie");
-  res.set(http::field::content_type, "text/html");
+    res.set(http::field::server, "http-beastie");
+    res.set(http::field::content_type, "text/html");
 
-  if (req.method() == http::verb::get) {
-    if (req.target() == "/") {
-      res.result(http::status::ok);
-      res.body() = "<h1 style=\"text-align: center;\">CSCE 1102</h1>";
+    if (req.method() == http::verb::get) {
+        std::string target = std::string(req.target());
+        if (target == "/") {
+            res.result(http::status::ok);
+            res.body() = read_file_to_string("static/index.html");
+        } else if (target == "/botros"){
+            res.result(http::status::ok);
+            res.body() = read_file_to_string("static/botros.html");
+        } else {
+            res.result(http::status::not_found);
+            res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
+        }
     } else {
-      res.result(http::status::not_found);
-      res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
+        res.result(http::status::method_not_allowed);
+        res.set(http::field::allow, "GET");
+        res.body() = "<h1 style=\"text-align: center;\">405 Method Not Allowed</h1>";
     }
-  } else {
-    res.result(http::status::method_not_allowed);
-    res.set(http::field::allow, "GET");
-    res.body() = "<h1 style=\"text-align: center;\">405 Method Not Allowed</h1>";
-  }
 
-  res.prepare_payload();
-  return res;
+    res.prepare_payload();
+    return res;
 }
