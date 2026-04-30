87cc525aff8ace6de7b68f651ccaaf8c7c9d80bd
Rola Kamel <Rkamel@aucegypt.edu>
2026-04-30 16:00:41 +0000

    updated hand_request function

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,7 +1,16 @@
 #include "utils.hpp"
-
+#include <fstream>
 namespace beast = boost::beast;
 namespace http = beast::http;
+
+std::string read_file(const std::string& path){
+	std::ifstream file(path);
+	std::string line;
+	std::string content;
+
+	while(std::getline(file, line)){
+	content+=line + "\n";}
+	return content;}
 
 http::response<http::string_body> handle_request(const http::request<http::string_body>& req) {
   http::response<http::string_body> res;
@@ -15,7 +24,7 @@ http::response<http::string_body> handle_request(const http::request<http::strin
   if (req.method() == http::verb::get) {
     if (req.target() == "/") {
       res.result(http::status::ok);
-      res.body() = "<h1 style=\"text-align: center;\">CSCE 1102</h1>";
+      res.body() = read_file("static/index.html");
     } else {
       res.result(http::status::not_found);
       res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
