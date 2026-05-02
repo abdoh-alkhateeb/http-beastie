78481c1ef3158a9b401544d2d375bb5a25537c12
christine selwanes <christineselwanes@aucegypt.edu>
2026-05-02 18:09:41 +0000

    Implement file reading, handle / and /selwanes endpoints

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,31 +1,63 @@
 #include "utils.hpp"
 
+#include <fstream>
+#include <sstream>
+
 namespace beast = boost::beast;
 namespace http = beast::http;
 
-http::response<http::string_body> handle_request(const http::request<http::string_body>& req) {
-  http::response<http::string_body> res;
 
-  res.version(req.version());
-  res.keep_alive(false);
+std::string read_file(const std::string& path) {
+    std::ifstream file(path);
 
-  res.set(http::field::server, "http-beastie");
-  res.set(http::field::content_type, "text/html");
-
-  if (req.method() == http::verb::get) {
-    if (req.target() == "/") {
-      res.result(http::status::ok);
-      res.body() = "<h1 style=\"text-align: center;\">CSCE 1102</h1>";
-    } else {
-      res.result(http::status::not_found);
-      res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
+    if (!file) {
+        return "";
     }
-  } else {
-    res.result(http::status::method_not_allowed);
-    res.set(http::field::allow, "GET");
-    res.body() = "<h1 style=\"text-align: center;\">405 Method Not Allowed</h1>";
-  }
 
-  res.prepare_payload();
-  return res;
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+
+    return buffer.str();
+}
+
+http::response<http::string_body> handle_request(const http::request<http::string_body>& req) {
+    http::response<http::string_body> res;
+
+    res.version(req.version());
+    res.keep_alive(false);
+
+    res.set(http::field::server, "http-beastie");
+    res.set(http::field::content_type, "text/html");
+
+    
+    if (req.method() == http::verb::get) {
+
+       
+        if (req.target() == "/") {
+            res.result(http::status::ok);
+            res.body() = read_file("static/index.html");
+        }
+
+        
+        else if (req.target() == "/selwanes") {   
+            res.result(http::status::ok);
+            res.body() = read_file("static/selwanes.html");
+        }
+
+        
+        else {
+            res.result(http::status::not_found);
+            res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
+        }
+    }
+
+   
+    else {
+        res.result(http::status::method_not_allowed);
+        res.set(http::field::allow, "GET");
+        res.body() = "<h1 style=\"text-align: center;\">405 Method Not Allowed</h1>";
+    }
+
+    res.prepare_payload();
+    return res;
 }
