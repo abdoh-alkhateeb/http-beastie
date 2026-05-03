6f67d1d39f43c723b827d551182e508af8904d71
Moayad <moayadxplayx@gmail.com>
2026-05-03 09:24:56 +0000

    Add file reading utility

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,31 +1,46 @@
 #include "utils.hpp"
 
+#include <fstream>
+#include <sstream>
+
 namespace beast = boost::beast;
 namespace http = beast::http;
 
-http::response<http::string_body> handle_request(const http::request<http::string_body>& req) {
-  http::response<http::string_body> res;
+std::string read_file(const std::string& path) {
+    std::ifstream file(path);
 
-  res.version(req.version());
-  res.keep_alive(false);
-
-  res.set(http::field::server, "http-beastie");
-  res.set(http::field::content_type, "text/html");
-
-  if (req.method() == http::verb::get) {
-    if (req.target() == "/") {
-      res.result(http::status::ok);
-      res.body() = "<h1 style=\"text-align: center;\">CSCE 1102</h1>";
-    } else {
-      res.result(http::status::not_found);
-      res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
+    if (!file.is_open()) {
+        return "";
     }
-  } else {
-    res.result(http::status::method_not_allowed);
-    res.set(http::field::allow, "GET");
-    res.body() = "<h1 style=\"text-align: center;\">405 Method Not Allowed</h1>";
-  }
 
-  res.prepare_payload();
-  return res;
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+
+    return buffer.str();
+}
+
+http::response<http::string_body> handle_request(const http::request<http::string_body>& req) {
+    http::response<http::string_body> res;
+
+    res.version(req.version());
+    res.keep_alive(false);
+    res.set(http::field::server, "http-beastie");
+    res.set(http::field::content_type, "text/html");
+
+    if (req.method() == http::verb::get) {
+        if (req.target() == "/") {
+            res.result(http::status::ok);
+            res.body() = "<h1 style=\"text-align: center;\">CSCE 1102</h1>";
+        } else {
+            res.result(http::status::not_found);
+            res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
+        }
+    } else {
+        res.result(http::status::method_not_allowed);
+        res.set(http::field::allow, "GET");
+        res.body() = "<h1 style=\"text-align: center;\">405 Method Not Allowed</h1>";
+    }
+
+    res.prepare_payload();
+    return res;
 }
