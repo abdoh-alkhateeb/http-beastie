10fdaaa757a716d338ef590646d51c15079f8001
malak <malakhosny101@gmail.com>
2026-05-07 21:31:18 +0300

    changed where it read from to read from static index.html

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,6 @@
 #include "utils.hpp"
 #include <fstream>
+#include <sstream>
 namespace beast = boost::beast;
 namespace http = beast::http;
  std::string read_file(const std::string& file_path) {
@@ -27,8 +28,7 @@ http::response<http::string_body> handle_request(const http::request<http::strin
   if (req.method() == http::verb::get) {
     if (req.target() == "/") {
       res.result(http::status::ok);
-      res.body() = "<h1 style=\"text-align: center;\">CSCE 1102</h1>";
-    } else {
+       res.body() = read_file("static/index.html");    } else {
       res.result(http::status::not_found);
       res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
     }
