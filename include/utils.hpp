beaa5dbbb9baec6775d77671dc84d34cd5c7da4a
root <root@DESKTOP-2907BKH.localdomain>
2026-05-07 23:42:14 +0300

    ADDED the utils function and modified the utils request function

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,5 +1,6 @@
 #pragma once
-
+#include <string>
 #include <boost/beast/http.hpp>
-
+using namespace std;
 boost::beast::http::response<boost::beast::http::string_body> handle_request(const boost::beast::http::request<boost::beast::http::string_body>& req);
+string readFile(const string& filePath);
