e5c1d2ec8f573308f56fc5566235bbaa2870d513
Omar Amer <omar4444yasser@gmail.com>
2026-05-07 17:33:15 +0300

    change body for html file when opening /

--- src/utils.cpp
+++ src/utils.cpp
@@ -7,7 +7,6 @@
 namespace beast = boost::beast;
 namespace http = beast::http;
 
-
 std::string read_file(const std::string& file_path) {
   std::ifstream file(file_path);
   std::stringstream text;
@@ -27,7 +26,7 @@ http::response<http::string_body> handle_request(const http::request<http::strin
   if (req.method() == http::verb::get) {
     if (req.target() == "/") {
       res.result(http::status::ok);
-      res.body() = "<h1 style=\"text-align: center;\">CSCE 1102</h1>";
+      res.body() = read_file("static/index.html");
     } else {
       res.result(http::status::not_found);
       res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
