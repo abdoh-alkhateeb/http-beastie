588722657757551713b38df2fd91b6d23e9f32a7
root <root@DESKTOP-2907BKH.localdomain>
2026-05-07 23:37:37 +0300

    Revert "Added the file read function and updated the request utility function to call said file read"
    This reverts commit 4728eb5efb9e4dc0c01fc10b39b5e60ffac6060d.

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
 #include <boost/beast/http.hpp>
-#include <string>
-using namespace std;
+
 boost::beast::http::response<boost::beast::http::string_body> handle_request(const boost::beast::http::request<boost::beast::http::string_body>& req);
-string readFile(const string& filePath);
