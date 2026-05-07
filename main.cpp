579543a087105e94e64a608ebc00ef6964b6d3ba
Hanya <hanyahashem@aucegypt.edu>
2026-05-07 19:15:57 +0000

    Implement file reading utility and test in main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include "utils.h"
 using namespace std;
 
 int main() {
     cout << "Server starting..." << endl;
-    return 0;
+    
+    string content = read_file("test.txt");
+
+    if (content.empty()) {
+        cout << "Failed to read file or file is empty." << endl;
+    } else {
+        cout << "File content:\n" << content << endl;
+    }    
+return 0;
 }
---
+++ utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+
+using namespace std;
+
+string read_file(const string& file_path);
+
+#endif
