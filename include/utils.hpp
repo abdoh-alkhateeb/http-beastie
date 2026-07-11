962e183df8d20ff5eda05e2081bb2062425b6375
Hazem Abuelalla <hazem020906@ucegypt.edu>
2026-07-11 19:19:26 +0300

    all done assignment4

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,5 +1,9 @@
 #pragma once
-
 #include <boost/beast/http.hpp>
+#include <string>
 
-boost::beast::http::response<boost::beast::http::string_body> handle_request(const boost::beast::http::request<boost::beast::http::string_body>& req);
+//  the minimal function to read files
+std::string read_file(const std::string& file_path);
+
+boost::beast::http::response<boost::beast::http::string_body> handle_request(
+    const boost::beast::http::request<boost::beast::http::string_body>& req);
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,29 +1,41 @@
 #include "utils.hpp"
 
+#include <fstream>
+#include <sstream>
+
 namespace beast = boost::beast;
 namespace http = beast::http;
 
-http::response<http::string_body> handle_request(const http::request<http::string_body>& req) {
+// Step d: Utility function to read file content into a string
+std::string read_file(const std::string& file_path) {
+  std::ifstream file(file_path);
+  if (!file.is_open()) return "<h1>404 Not Found</h1>";
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  return buffer.str();
+}
+
+http::response<http::string_body> handle_request(
+    const http::request<http::string_body>& req) {
   http::response<http::string_body> res;
-
   res.version(req.version());
-  res.keep_alive(false);
-
   res.set(http::field::server, "http-beastie");
   res.set(http::field::content_type, "text/html");
 
-  if (req.method() == http::verb::get) {
-    if (req.target() == "/") {
-      res.result(http::status::ok);
-      res.body() = "<h1 style=\"text-align: center;\">CSCE 1102</h1>";
-    } else {
-      res.result(http::status::not_found);
-      res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
-    }
+  // Step e
+  if (req.method() == http::verb::get && req.target() == "/") {
+    res.result(http::status::ok);
+    res.body() = read_file("static/index.html");
+
+    // Step f
+  } else if (req.method() == http::verb::get && req.target() == "/Abuelalla") {
+    res.result(http::status::ok);
+    res.body() = read_file("static/Abuelalla.html");
+
+    // Catch fallback for unrecognized URLs
   } else {
-    res.result(http::status::method_not_allowed);
-    res.set(http::field::allow, "GET");
-    res.body() = "<h1 style=\"text-align: center;\">405 Method Not Allowed</h1>";
+    res.result(http::status::not_found);
+    res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
   }
 
   res.prepare_payload();
